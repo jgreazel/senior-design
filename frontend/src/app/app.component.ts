f766b307479e9d85f536abf1f2b7f0c96670de62
Jon <jdgreazel@gmail.com>
2021-02-10 22:33:31 -0600

    palette mostly done, may need tweeks or refactoring

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -17,19 +17,19 @@ export class AppComponent {
 
 
   // TEST POST METHOD
-  computeNodeData(){
+  computeNodeData() {
     let rando = Math.random();
-    this.apiService.computeNodeData({"id": rando, "key": rando, "color": "red"})
+    this.apiService.computeNodeData({ "id": rando, "key": rando, "color": "red" })
       .subscribe(data => {
         console.log(data);
       })
   }
 
   // TEST GET METHOD
-  getNodeData(){
+  getNodeData() {
     this.apiService.getNodeData()
       .subscribe(data => {
-        console.log('nodes',data);
+        console.log('nodes', data);
         this.diagramNodeData = data
         // this.diagramLinkData = null;
       })
@@ -38,20 +38,67 @@ export class AppComponent {
   // initialize diagram / templates
   public initDiagram(): go.Diagram {
 
+    //create custom shape for and-gate
+    go.Shape.defineFigureGenerator("AndGate", function (shape, w, h) {
+      var geo = new go.Geometry();
+      var cpOffset = (4 * ((Math.sqrt(2) - 1) / 3)) * .5;
+      var fig = new go.PathFigure(0, 0, true);
+      geo.add(fig);
+      h = 2*h;
+      w = 1.5 * w;
+      // The gate body
+      fig.add(new go.PathSegment(go.PathSegment.Line, .5 * w, 0));
+      fig.add(new go.PathSegment(go.PathSegment.Bezier, w, .5 * h, (.5 + cpOffset) * w, 0,
+        w, (.5 - cpOffset) * h));
+      fig.add(new go.PathSegment(go.PathSegment.Bezier, .5 * w, h, w, (.5 + cpOffset) * h,
+        (.5 + cpOffset) * w, h));
+      fig.add(new go.PathSegment(go.PathSegment.Line, 0, h).close());
+
+      geo.rotate(270, w/3, h/2)
+      geo.spot1 = go.Spot.TopLeft;
+      geo.spot2 = go.Spot.BottomRight;
+      return geo;
+    })
+
+    //create custom shape for or-gate
+    go.Shape.defineFigureGenerator("OrGate", function(shape, w, h) {
+      var geo = new go.Geometry();
+      var radius = .5;
+      var cpOffset = (4 * ((Math.sqrt(2) - 1) / 3)) * radius;
+      var centerx = 0;
+      var centery = .5;
+      var fig = new go.PathFigure(0, 0, true);
+      geo.add(fig);
+      h=2*h;
+      w=1.5*w;
+    
+      fig.add(new go.PathSegment(go.PathSegment.Bezier, w, .5 * h, (centerx + cpOffset + cpOffset) * w, (centery - radius) * h,
+        .8 * w, (centery - cpOffset) * h));
+      fig.add(new go.PathSegment(go.PathSegment.Bezier, 0, h, .8 * w, (centery + cpOffset) * h,
+        (centerx + cpOffset + cpOffset) * w, (centery + radius) * h));
+      fig.add(new go.PathSegment(go.PathSegment.Bezier, 0, 0, .25 * w, .75 * h, .25 * w, .25 * h).close());
+      geo.rotate(270, w/3, h/2)
+      geo.spot1 = new go.Spot(.2, .25);
+      geo.spot2 = new go.Spot(.75, .75);
+      return geo;
+    });
+
     const $ = go.GraphObject.make;
     const dia = $(go.Diagram, {
       initialAutoScale: go.Diagram.UniformToFill,
       layout: $(go.TreeLayout,
-        { comparer: go.LayoutVertex.standardComparer, 
-          
-        angle: 90}) // have the comparer sort by numbers as well as letters
+        {
+          comparer: go.LayoutVertex.standardComparer,
+
+          angle: 90
+        }) // have the comparer sort by numbers as well as letters
       // other properties are set by the layout function, defined below
     });
 
     dia.commandHandler.archetypeGroupData = { key: 'Group', isGroup: true };
 
 
-    const makePort = function(id: string, spot: go.Spot) {
+    const makePort = function (id: string, spot: go.Spot) {
       return $(go.Shape, 'Circle',
         {
           opacity: .5,
@@ -70,8 +117,8 @@ export class AppComponent {
             $('ContextMenu',
               $('ContextMenuButton',
                 $(go.TextBlock, 'Group'),
-                { click: function(e, obj) { e.diagram.commandHandler.groupSelection(); } },
-                new go.Binding('visible', '', function(o) {
+                { click: function (e, obj) { e.diagram.commandHandler.groupSelection(); } },
+                new go.Binding('visible', '', function (o) {
                   return o.diagram.selection.count > 1;
                 }).ofObject())
             )
@@ -104,7 +151,7 @@ export class AppComponent {
   public skipsDiagramUpdate = false;
 
   // When the diagram model changes, update app data to reflect those changes
-  public diagramModelChange = function(changes: go.IncrementalData) {
+  public diagramModelChange = function (changes: go.IncrementalData) {
     // when setting state here, be sure to set skipsDiagramUpdate: true since GoJS already has this update
     // (since this is a GoJS model changed listener event function)
     // this way, we don't log an unneeded transaction in the Diagram's undoManager history
@@ -115,24 +162,27 @@ export class AppComponent {
     this.diagramModelData = DataSyncService.syncModelData(changes, this.diagramModelData);
   };
 
-
-
   public initPalette(): go.Palette {
     const $ = go.GraphObject.make;
     const palette = $(go.Palette);
 
-    // define the Node template
     palette.nodeTemplate =
       $(go.Node, 'Auto',
-        $(go.Shape, 'Square',
-          {
-            stroke: null
-          },
-          new go.Binding('fill', 'color')
-        ),
-        $(go.TextBlock, { margin: 8 },
-          new go.Binding('text', 'key'))
-      );
+        // $(go.Shape, {
+        //   figure: 'circle', strokeWidth: 3, fill: 'white'
+        // }, new go.Binding('figure', 'shape'), new go.Binding('stroke', 'color')),
+        // $(go.TextBlock, {
+        //   font: 'bold 20px Courier', textAlign: 'center', margin: 10
+        // }, new go.Binding('text', 'key')),
+        $(go.Panel, "Vertical",
+        $(go.Shape, {
+          figure: 'circle', strokeWidth: 3, fill: 'white',  alignment: go.Spot.Bottom, stretch: go.GraphObject.Horizontal,
+          height: 40, width: 45 , margin: 20
+        }, new go.Binding('figure', 'shape'), new go.Binding('stroke', 'color')),
+        $(go.TextBlock, {
+          font: 'bold 20px Courier', margin: 5
+        }, new go.Binding('text', 'key')))
+      )
 
     palette.model = $(go.GraphLinksModel,
       {
@@ -142,18 +192,18 @@ export class AppComponent {
     return palette;
   }
   public paletteNodeData: Array<go.ObjectData> = [
-    { key: 'ROOT', color: 'lightblue'},
-    { key: 'AND', color: 'red' },
-    { key: 'OR', color: 'lightgreen' },
-    { key: 'LEAF', color: 'lightgrey' }
+    { key: 'ROOT', color: 'lightblue' },
+    { key: 'AND', color: 'red', shape: 'andgate' },
+    { key: 'OR', color: 'lightgreen', shape: 'orgate' },
+    { key: 'LEAF', color: 'black' }
   ];
   public paletteLinkData: Array<go.ObjectData> = [
-    {  }
+    {}
   ];
   public paletteModelData = { prop: 'val' };
   public paletteDivClassName = 'myPaletteDiv';
   public skipsPaletteUpdate = false;
-  public paletteModelChange = function(changes: go.IncrementalData) {
+  public paletteModelChange = function (changes: go.IncrementalData) {
     // when setting state here, be sure to set skipsPaletteUpdate: true since GoJS already has this update
     // (since this is a GoJS model changed listener event function)
     // this way, we don't log an unneeded transaction in the Palette's undoManager history
@@ -187,7 +237,7 @@ export class AppComponent {
 
     const appComp: AppComponent = this;
     // listener for inspector
-    this.myDiagramComponent.diagram.addDiagramListener('ChangedSelection', function(e) {
+    this.myDiagramComponent.diagram.addDiagramListener('ChangedSelection', function (e) {
       if (e.diagram.selection.count === 0) {
         appComp.selectedNode = null;
       }
