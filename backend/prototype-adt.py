3242a0d9c262f930f2fa252b0db85fe1e1354a09
Stefan Peng <stefanpeng9@gmail.com>
2021-02-28 18:46:03 -0600

    Add initial game theory analysis

--- backend/prototype-adt.py
+++ backend/prototype-adt.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import nashpy as nash
 
 
 class Scenario:
@@ -42,10 +43,16 @@ class Scenario:
         risk = str(round(self.impact * self.probability, 3))
         prob = str(round(self.probability, 4))
         impact = str(round(self.impact, 2))
-        output = "risk: " + risk + "  \tprob: " + prob + "  \timpact: " + impact + "\t: "
+        output = (
+            "risk: " + risk + "  \tprob: " + prob + "  \timpact: " + impact + "\t: "
+        )
         for key in self.nodeKeys:
             output += key + " "
         return output
+
+    def get_cost(self):
+        cost = self.probability * self.impact
+        return cost
 
     def combine(self, scenario2):
         """
@@ -64,6 +71,7 @@ class Scenario:
         for key in scenario2.nodeKeys:
             keys.append(key)
         return Scenario(prob, impact, keys)
+
 
 def normalize():
     """
@@ -95,13 +103,14 @@ def findRoot():
         print("Error:: Cannot find root node")
     return root
 
+
 def findAttackRoot():
     """Find root node of attack tree (that does not include safe path node)."""
     root = findRoot()
     children = findChildren(root)
     for node in children:
-      if node["key"][0] != "L":
-        return node
+        if node["key"][0] != "L":
+            return node
     print("Error:: Cannot find attack root node")
     return root
 
@@ -139,7 +148,7 @@ def findChildren(node):
 
 def findScenarios(node):
     """
-    Recusive function for finding all scenarios from a given node.
+    Recursive function for finding all scenarios from a given node.
 
     Parameters
     ----------
@@ -152,7 +161,11 @@ def findScenarios(node):
         scenarioList.append(Scenario(node["riskIndex"], node["impact"], [node["key"]]))
         defense = findChildren(node)
         if len(defense) > 0:
-            defenseList.append(Scenario(defense[0]["riskIndex"], defense[0]["impact"], [defense[0]["key"]]))
+            defenseList.append(
+                Scenario(
+                    defense[0]["riskIndex"], defense[0]["impact"], [defense[0]["key"]]
+                )
+            )
         return scenarioList, defenseList
     elif node["key"][0] == "O":  # If OR node
         scenarioList = list(())
@@ -168,7 +181,9 @@ def findScenarios(node):
             for scenario in childScenarios:
                 scenarioList.append(scenario)
         defenseScenarioList = childDefenseLists[0]
-        for i in range(1, len(childDefenseLists)):  # Compare all combinations of scenarios
+        for i in range(
+            1, len(childDefenseLists)
+        ):  # Compare all combinations of scenarios
             for scenario1 in defenseScenarioList:
                 for scenario2 in childDefenseLists[i]:
                     tempList.append(scenario1.combine(scenario2))
@@ -200,178 +215,197 @@ def findScenarios(node):
         print("Error:: Could not determine node type")
 
 
+def nasheq(impact, attack_costs, defense_costs):
+    """
+    Computes the nash equilibrium for the given impacts, attack costs,
+    and defense costs. Currently expects 1D parameters of equal length.
+
+    Returns an equilibria generator.
+    """
+    payoff_attacker = []
+    for i in range(len(attack_costs)):
+        payoff_row = []
+        for j in range(len(defense_costs)):
+            payoff_row.append(defense_costs[j] + impact - attack_costs[i])
+        payoff_attacker.append(payoff_row)
+
+    game = nash.Game(payoff_attacker)
+    eqs = game.support_enumeration(non_degenerate=False, tol=0)
+    return eqs
+
+
 # Get object from JSON to List
 jsonObj = """
 {
-  "nodeData": [
-    {
-      "key": "OR3",
-      "text": "placeholderText",
-      "riskIndex": "0",
-      "color": "red",
-      "shape": "andgate"
-    },
-    {
-      "key": "LEAF5",
-      "text": "safePath",
-      "riskIndex": "8"
-    },
-    {
-      "key": "AND",
-      "text": "placeholderText",
-      "riskIndex": "0",
-      "color": "red",
-      "shape": "andgate"
-    },
-    {
-      "key": "OR",
-      "text": "placeholderText",
-      "riskIndex": "0",
-      "color": "lightgreen",
-      "shape": "orgate"
-    },
-    {
-      "key": "LEAF",
-      "text": "cyberAttack",
-      "riskIndex": "1.1",
-      "impact": "89"
-    },
-    {
-      "key": "LEAF2",
-      "text": "physicalAttack",
-      "riskIndex": "8.3",
-      "impact": "11"
-    },
-    {
-      "key": "OR2",
-      "text": "placeholderText",
-      "riskIndex": "0",
-      "color": "lightgreen",
-      "shape": "orgate"
-    },
-    {
-      "key": "LEAF3",
-      "text": "phishingAttack",
-      "riskIndex": "7.2",
-      "impact": "231"
-    },
-    {
-      "key": "LEAF4",
-      "text": "spyware",
-      "riskIndex": "3.4",
-      "impact": "20"
-    },
-    {
-      "key": "DEFENSE3",
-      "text": "Defense 3",
-      "riskIndex": "1",
-      "impact": "4"
-    },
-    {
-      "key": "DEFENSE4",
-      "text": "Defense 4",
-      "riskIndex": "1",
-      "impact": "5"
-    },
-    {
-      "key": "DEFENSE2",
-      "text": "Defense 2",
-      "riskIndex": "1",
-      "impact": "3"
-    },
-    {
-      "key": "DEFENSE1",
-      "text": "Defense 1",
-      "riskIndex": "1",
-      "impact": "2"
-    }
-  ],
-  "edgeData": [
-    {
-      "from": "OR3",
-      "to": "AND",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -7
-    },
-    {
-      "from": "OR3",
-      "to": "LEAF5",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -8
-    },
-    {
-      "from": "AND",
-      "to": "OR",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -1
-    },
-    {
-      "from": "OR",
-      "to": "LEAF",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -2
-    },
-    {
-      "from": "OR",
-      "to": "LEAF2",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -3
-    },
-    {
-      "from": "AND",
-      "to": "OR2",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -4
-    },
-    {
-      "from": "OR2",
-      "to": "LEAF3",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -5
-    },
-    {
-      "from": "OR2",
-      "to": "LEAF4",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -6
-    },
-    {
-      "from": "LEAF4",
-      "to": "DEFENSE4",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -7
-    },
-    {
-      "from": "LEAF3",
-      "to": "DEFENSE3",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -7
-    },
-    {
-      "from": "LEAF2",
-      "to": "DEFENSE2",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -8
-    },
-    {
-      "from": "LEAF",
-      "to": "DEFENSE1",
-      "fromPort": "b",
-      "toPort": "t",
-      "key": -9
-    }
-  ]
+    "nodeData": [
+        {
+            "key": "OR3",
+            "text": "placeholderText",
+            "riskIndex": "0",
+            "color": "red",
+            "shape": "andgate"
+        },
+        {
+            "key": "LEAF5",
+            "text": "safePath",
+            "riskIndex": "8"
+        },
+        {
+            "key": "AND",
+            "text": "placeholderText",
+            "riskIndex": "0",
+            "color": "red",
+            "shape": "andgate"
+        },
+        {
+            "key": "OR",
+            "text": "placeholderText",
+            "riskIndex": "0",
+            "color": "lightgreen",
+            "shape": "orgate"
+        },
+        {
+            "key": "LEAF",
+            "text": "cyberAttack",
+            "riskIndex": "1.1",
+            "impact": "89"
+        },
+        {
+            "key": "LEAF2",
+            "text": "physicalAttack",
+            "riskIndex": "8.3",
+            "impact": "11"
+        },
+        {
+            "key": "OR2",
+            "text": "placeholderText",
+            "riskIndex": "0",
+            "color": "lightgreen",
+            "shape": "orgate"
+        },
+        {
+            "key": "LEAF3",
+            "text": "phishingAttack",
+            "riskIndex": "7.2",
+            "impact": "231"
+        },
+        {
+            "key": "LEAF4",
+            "text": "spyware",
+            "riskIndex": "3.4",
+            "impact": "20"
+        },
+        {
+            "key": "DEFENSE3",
+            "text": "Defense 3",
+            "riskIndex": "1",
+            "impact": "4"
+        },
+        {
+            "key": "DEFENSE4",
+            "text": "Defense 4",
+            "riskIndex": "1",
+            "impact": "5"
+        },
+        {
+            "key": "DEFENSE2",
+            "text": "Defense 2",
+            "riskIndex": "1",
+            "impact": "3"
+        },
+        {
+            "key": "DEFENSE1",
+            "text": "Defense 1",
+            "riskIndex": "1",
+            "impact": "6.1"
+        }
+    ],
+    "edgeData": [
+        {
+            "from": "OR3",
+            "to": "AND",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -7
+        },
+        {
+            "from": "OR3",
+            "to": "LEAF5",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -8
+        },
+        {
+            "from": "AND",
+            "to": "OR",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -1
+        },
+        {
+            "from": "OR",
+            "to": "LEAF",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -2
+        },
+        {
+            "from": "OR",
+            "to": "LEAF2",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -3
+        },
+        {
+            "from": "AND",
+            "to": "OR2",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -4
+        },
+        {
+            "from": "OR2",
+            "to": "LEAF3",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -5
+        },
+        {
+            "from": "OR2",
+            "to": "LEAF4",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -6
+        },
+        {
+            "from": "LEAF4",
+            "to": "DEFENSE4",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -7
+        },
+        {
+            "from": "LEAF3",
+            "to": "DEFENSE3",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -7
+        },
+        {
+            "from": "LEAF2",
+            "to": "DEFENSE2",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -8
+        },
+        {
+            "from": "LEAF",
+            "to": "DEFENSE1",
+            "fromPort": "b",
+            "toPort": "t",
+            "key": -9
+        }
+    ]
 }
 """
 jsonData = json.loads(jsonObj)
@@ -384,3 +418,9 @@ normalize()
 scenarios, defenses = findScenarios(findAttackRoot())
 print(*scenarios, sep="\n")
 print(*defenses, sep="\n")
+
+attackCosts = [scenario.get_cost() for scenario in scenarios]
+defenseCosts = [scenario.get_cost() for scenario in defenses]
+
+eqs = nasheq(10, attackCosts, defenseCosts)
+print(list(eqs))
